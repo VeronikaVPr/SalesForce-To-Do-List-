36e7b7056cd2f66376b718067e42a2612f6d5688
VeronikaVPr <sskkvvpodpiska112@gmail.com>
2021-11-04 14:14:08 +0200

    all project

--- force-app/main/default/lwc/personRecordPage/personRecordPage.js
+++ force-app/main/default/lwc/personRecordPage/personRecordPage.js
@@ -1,11 +1,42 @@
-import { LightningElement , track} from 'lwc';
+import { LightningElement ,api, wire, track} from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+
 import getUpcomingTaskCurrentPerson from '@salesforce/apex/PersonRecordPageController.getUpcomingTaskCurrentPerson';
+import pesSyncOut from '@salesforce/apex/SyncOutPerson.pesSyncOut';
+
+import Id from '@salesforce/schema/Person__c.Id';
+const FIELDS = ['Task__c.Name', 'Task__c.Start_Date__c'];
 
 export default class PersonRecordPage extends LightningElement {
-  clickedButtonLabel;
-
-  handleClick(event) {
-      this.clickedButtonLabel ='Person Sync is not yet configured';
-  }
   
-}
+  @track columns = [{
+    label: 'Task name',
+    fieldName: 'Name',
+    type: 'text',
+   },
+  {
+    label: 'Start Date',
+    fieldName: 'Start_Date__c',
+    type: 'date',
+  }
+];
+  
+  @api recordId;
+ 
+  @wire(getUpcomingTaskCurrentPerson,{ personId: '$recordId' })  tasks;
+
+  @track clickedButtonLabel;
+  
+  handleClick(event) {
+
+      pesSyncOut({
+        personId: this.recordId
+      })
+      .then(response => {
+        alert(response);
+      })
+      .catch(error => console.error(error.body.message))
+
+  }
+    
+  }
